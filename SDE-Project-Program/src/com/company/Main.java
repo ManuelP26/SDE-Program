31cc81e9f744cd2321ea0e38ad4605d255cd7dca
ManuelP26 <popp0050@hz.nl>
2022-01-23 22:35:22 +0100

    add main and merge conflict gone

--- SDE-Project-Program/src/com/company/Main.java
+++ SDE-Project-Program/src/com/company/Main.java
@@ -1,1 +1,11 @@
+package com.company;
 
+public class Main {
+
+    public static void main(String[] args) {
+        carFacade carFacade = new carFacade(0);
+        while (true){
+            carFacade.carPicker();
+        }
+    }
+}
